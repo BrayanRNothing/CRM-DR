676ea5e68c2dd6606b2a43b624243a5b22d8ebc9
Julio <julio@protexinnovation.com>
2026-03-23 12:16:43 -0600

    feat: agrega seguimiento de prospectos en menu de clientes

--- src/App.jsx
+++ src/App.jsx
@@ -130,7 +130,7 @@ function App() {
             <Route path="prospectos" element={<ProspectorSeguimiento />} />
             <Route path="calendario" element={<ProspectorCalendario />} />
             <Route path="clientes" element={<CRMClientes />} />
-            <Route path="clientes/seguimiento" element={<CRMClientes />} />
+            <Route path="clientes/seguimiento" element={<ProspectorSeguimiento />} />
             <Route path="usuarios/prospectors" element={<UserManagement initialRole="prospector" />} />
             <Route path="usuarios/closers" element={<UserManagement initialRole="closer" />} />
             <Route path="usuarios/vendedores" element={<UserManagement initialRole="vendedor" />} />
@@ -143,7 +143,7 @@ function App() {
             <Route path="calendario" element={<CloserCalendario />} />
             <Route path="prospectos" element={<ProspectorSeguimiento />} />
             <Route path="clientes" element={<CRMClientes />} />
-            <Route path="clientes/seguimiento" element={<CRMClientes />} />
+            <Route path="clientes/seguimiento" element={<ProspectorSeguimiento />} />
             <Route path="usuarios/prospectors" element={<UserManagement initialRole="prospector" />} />
             <Route path="usuarios/closers" element={<UserManagement initialRole="closer" />} />
             <Route path="usuarios/vendedores" element={<UserManagement initialRole="vendedor" />} />
@@ -158,7 +158,7 @@ function App() {
             <Route path="calendario" element={<ProspectorCalendario />} />
             <Route path="prospectos" element={<ProspectorSeguimiento />} />
             <Route path="clientes" element={<CRMClientes />} />
-            <Route path="clientes/seguimiento" element={<CRMClientes />} />
+            <Route path="clientes/seguimiento" element={<ProspectorSeguimiento />} />
             <Route path="usuarios/prospectors" element={<UserManagement initialRole="prospector" />} />
             <Route path="usuarios/closers" element={<UserManagement initialRole="closer" />} />
             <Route path="usuarios/vendedores" element={<UserManagement initialRole="vendedor" />} />
